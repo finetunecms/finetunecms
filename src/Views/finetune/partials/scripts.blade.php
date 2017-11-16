75656b0feb347f32049331437acf78d02fac6c6c
Christopher Thompson <chris@homercreative.com>
2017-11-16 10:37:26 +0000

    JS versioning and SCSS

--- src/Views/finetune/partials/scripts.blade.php
+++ src/Views/finetune/partials/scripts.blade.php
@@ -1,5 +1,5 @@
 <script src="/finetune/assets/js/tinymce/tinymce.js"></script>
 <script src="/finetune/assets/js/lazyload.min.js"></script>
-<script src="{{ mix("js/manifest.js", "/inetune/assets/") }}"></script>
+<script src="{{ mix("js/manifest.js", "finetune/assets/") }}"></script>
 <script src="{{ mix("js/vendor.js", "finetune/assets/") }}"></script>
 <script src="{{ mix("js/app.js", "finetune/assets/") }}"></script>
