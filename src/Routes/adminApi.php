274cb2408268e48ad2e6cf8e64c244e0ad690710
cyberstorm305 <chris@homercreative.com>
2018-03-15 15:36:10 +0000

    Finetune snippets custom fields and rendering nods

--- src/Routes/adminApi.php
+++ src/Routes/adminApi.php
@@ -58,7 +58,7 @@ Route::group(['middleware' => ['ability:Superadmin,can_administer_website'], 'na
         Route::resource('snippetgroups', 'Admin\Api\SnippetGroupController');
         Route::post('snippets/publish', 'Admin\Api\SnippetController@publish');
         Route::post('snippets/order', 'Admin\Api\SnippetController@saveOrder');
-        Route::get('snippets/fields/{groupId]', 'Admin\Api\SnippetController@getFields');
+        Route::get('snippets/fields/{groupId}', 'Admin\Api\SnippetController@getFields');
         Route::post('snippets/fields', 'Admin\Api\SnippetController@postFields');
         Route::resource('snippets', 'Admin\Api\SnippetController');
 
